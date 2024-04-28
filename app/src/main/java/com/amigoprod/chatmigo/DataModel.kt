83a04bbc1ddc2aa8b479b14a6818de8f11c9673d
Armaan <mohammadarmaan533@gmail.com>
2024-04-28 23:20:46 +0530

    Fixing and adding data models

--- app/src/main/java/com/amigoprod/chatmigo/DataModel.kt
+++ app/src/main/java/com/amigoprod/chatmigo/DataModel.kt
@@ -1,13 +1,40 @@
 package com.amigoprod.chatmigo
 
+import android.system.Int64Ref
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 import java.sql.Timestamp
 
-class Message(
-    @SerializedName("id") @Expose var id: Int? = null,
-    @SerializedName("msg") @Expose var msg: String? = null,
-    @SerializedName("timestamp") @Expose var timestamp: Timestamp? = null,
-    @SerializedName("delivered") @Expose var delivered: Boolean = false,
-    @SerializedName("read") @Expose var read: Boolean = false
+//class Message(
+//    @SerializedName("id") @Expose var id: Long? = null,
+//    @SerializedName("content") @Expose var content: String? = null,
+//    @SerializedName("timestamp") @Expose var timestamp: Timestamp? = null,
+//    @SerializedName("delivered") @Expose var delivered: Boolean = false,
+//    @SerializedName("read") @Expose var read: Boolean = false
+//)
+
+data class Message(
+    val mid: String,
+    val content: String? = null,
+    val sender: User? = null,
+    val timestamp: Timestamp? = null,
+    val delivered: Boolean = false,
+    val read: Boolean = false
 )
+
+data class Chat(
+    val cid: String,
+    val messages: List<Message>? = emptyList(),
+    val members: List<User>? = emptyList()
+)
+
+data class User(
+    val uid: String,
+    val username: String? = null,
+    val phone: String,
+    val password: String? = null
+)
+
+fun Message.isMine(user: User): Boolean {
+    return this.sender!!.uid == user.uid
+}
