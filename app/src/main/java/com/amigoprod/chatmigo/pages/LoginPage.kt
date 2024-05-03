1d8cf88cc5d4fca21e6c97f7b5d40663ac933cec
Armaan <mohammadarmaan533@gmail.com>
2024-05-03 06:50:06 +0530

    Added a login page

---
+++ app/src/main/java/com/amigoprod/chatmigo/pages/LoginPage.kt
@@ -0,0 +1,13 @@
+package com.amigoprod.chatmigo.pages
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+
+@Preview
+@Composable
+fun LoginPage(
+    navController: NavController? = null
+) {
+    
+}
